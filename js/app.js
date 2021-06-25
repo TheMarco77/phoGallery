b21f0698904fe3dc1affece18b59cbca1a0143d9
Marco Ramos <mfilipe@gmail.com>
2021-06-25 01:11:20 -0400

    keyup-event-listener

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
-const baguetteBoxMin = require("./baguetteBox.min");
-const search = new filter('search', 'data-caption');
-baguetteBoxMin.run('.gallery');
+baguetteBox.run('.gallery', {
+    "captions": true,
+    "buttons": true,
+    "overlayBackgroundColor": "rgb(0, 0, 0, 0.8)"
+});
+document.getElementById("query").addEventListener("keyup", myFunction);
+
+function myFunction() {
+    let x = document.getElementById("query");
+    x.value = x.value.toLowerCase();
+}
+
+// const search = new filter('search', 'data-caption');

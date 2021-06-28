0d24b5665a549cf4cc57b16251e8a224f322a002
Marco Ramos <mfilipe@gmail.com>
2021-06-28 18:30:48 -0400

    Revert "Code Review"
    This reverts commit a47aff37140d03fcb95cd9db3fe728d0ff545072.

--- js/app.js
+++ js/app.js
@@ -3,22 +3,11 @@ baguetteBox.run('.gallery', {
     "buttons": true,
     "overlayBackgroundColor": "rgb(0, 0, 0, 0.8)"
 });
-
 document.getElementById("query").addEventListener("keyup", myFunction);
 
-// line 11 makes the search available if user copy paste into the search Box
-document.getElementById("query").addEventListener("input", myFunction);
 function myFunction() {
-    let searchBox = document.getElementById("query");
-    let searchTerm = searchBox.value.toLowerCase();
-    let picLinks = document.querySelectorAll(".gallery a");
-
-    for ( let picture of picLinks) { 
-        let caption = picture.getAttribute("data-caption").toLowerCase();
-        if (caption.includes(searchTerm)){
-            picture.style.display = "inherit";
-        } else {
-            picture.style.display = "none";
-        }
-    }
+    let x = document.getElementById("query");
+    x.value = x.value.toLowerCase();
 }
+
+// const search = new filter('search', 'data-caption');
